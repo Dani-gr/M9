b767c2bada486dde1b60c137d241a0f59cff364c
carloscmor <100674357+carloscmor@users.noreply.github.com>
2023-03-20 13:51:00 +0100

    Proyecto BancApp

---
+++ BancApp/src/main/java/es/proyectotaw/banca/bancapp/BancAppApplication.java
@@ -0,0 +1,13 @@
+package es.proyectotaw.banca.bancapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BancAppApplication.class, args);
+    }
+
+}
---
+++ BancApp/src/test/java/es/proyectotaw/banca/bancapp/BancAppApplicationTests.java
@@ -0,0 +1,13 @@
+package es.proyectotaw.banca.bancapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
