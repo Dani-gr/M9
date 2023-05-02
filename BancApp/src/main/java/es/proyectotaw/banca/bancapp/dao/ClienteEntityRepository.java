71e30de63ba9571d7ed7a3886b8e8faa253eb3f1
Nu36 <100622207+Nu36@users.noreply.github.com>
2023-05-02 20:41:47 +0200

    Update ClienteEntityRepository.java

--- BancApp/src/main/java/es/proyectotaw/banca/bancapp/dao/ClienteEntityRepository.java
+++ BancApp/src/main/java/es/proyectotaw/banca/bancapp/dao/ClienteEntityRepository.java
@@ -4,4 +4,6 @@ import es.proyectotaw.banca.bancapp.entity.ClienteEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClienteEntityRepository extends JpaRepository<ClienteEntity, Integer> {
+    @Query("select c from ClienteEntity c where c.idCliente = :id")
+    public ClienteEntity buscarPorID(@Param("id") Integer id);
 }
