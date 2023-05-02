d745e111e2103479a4b2aa98321590cab5532f69
Dani-gr <dani.gr@uma.es>
2023-05-02 02:14:41 +0200

    Deleted role view and renamed LoginController

--- BancApp/src/main/java/es/proyectotaw/banca/bancapp/controller/LoginController.java
+++ BancApp/src/main/java/es/proyectotaw/banca/bancapp/controller/LoginController.java
@@ -1,7 +1,5 @@
 package es.proyectotaw.banca.bancapp.controller;
 
-import es.proyectotaw.banca.bancapp.dao.RolEntityRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,17 +12,7 @@ import javax.servlet.http.HttpSession;
 @SuppressWarnings("SpringMVCViewInspection")
 @Controller
 @RequestMapping("/")
-public class RolController {
-    @Autowired
-    RolEntityRepository rolEntityRepository;
-
-    // TODO remove
-    @GetMapping("/testRoles")
-    String doTest(Model model) {
-        model.addAttribute("roles", rolEntityRepository.findAll());
-        return "vistaRoles";
-    }
-
+public class LoginController {
     @GetMapping("/")
     String doLogin(Model model, @ModelAttribute("entidad") String entidad, @ModelAttribute("cifEmpresa") String cif,
                    @ModelAttribute("user") String email) {
