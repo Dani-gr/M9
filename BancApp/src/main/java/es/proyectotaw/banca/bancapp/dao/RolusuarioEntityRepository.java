950b43de97d09bc5bc0f19bcdfa48c6c357a64c2
Dani-gr <dani.gr@uma.es>
2023-05-07 19:21:14 +0200

    Actualizado /reset y login de empresa, y actualizada vista combinada de cajero

--- BancApp/src/main/java/es/proyectotaw/banca/bancapp/dao/RolusuarioEntityRepository.java
+++ BancApp/src/main/java/es/proyectotaw/banca/bancapp/dao/RolusuarioEntityRepository.java
@@ -11,6 +11,10 @@ import java.util.List;
 
 public interface RolusuarioEntityRepository extends JpaRepository<RolusuarioEntity, Integer> {
     @Query("select rolusuariosById.rolByIdrol from RolusuarioEntity rolusuariosById " +
-            "where rolusuariosById.usuarioByIdusuario=:user and rolusuariosById.empresaByIdempresa.cif = :cif")
+            "where rolusuariosById.usuarioByIdusuario = :user and rolusuariosById.empresaByIdempresa.cif = :cif")
     List<RolEntity> findRolesByUsuarioAndEmpresaByCif(@Param("user") UsuarioEntity user, @Param("cif") Integer cif);
+
+    @Query("select rolusuariosById.rolByIdrol from RolusuarioEntity rolusuariosById " +
+            "where rolusuariosById.usuarioByIdusuario = :user and rolusuariosById.empresaByIdempresa is null")
+    List<RolEntity> findRolesByUsuarioNoEmpresa(@Param("user") UsuarioEntity user);
 }
